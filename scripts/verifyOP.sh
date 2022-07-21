07e7dab79b4e4407b236875523c65769d5776865
Yura Sherman <yurasherman+github@gmail.com>
2022-07-21 10:53:12 +0300

    feat: OP token

---
+++ scripts/verifyOP.sh
@@ -0,0 +1,15 @@
+if [[ "$1" ]]
+then
+    RULE="--rule $1"
+fi
+
+
+certoraRun contracts/OP.sol:GovernanceToken  \
+    --verify GovernanceToken:spec/erc20.spec $RULE  \
+    --solc solc8.0 \
+    --staging \
+    --send_only \
+    --optimistic_loop \
+    --settings -enableEqualitySaturation=false,-solver=z3,-smt_usePz3=true,-smt_z3PreprocessorTimeout=2 \
+    --solc_args '["--experimental-via-ir"]' \
+    --msg "Optimism:erc20.spec $1"
